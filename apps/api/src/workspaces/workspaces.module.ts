2747a33c8da82f0cc98a8c20ffc9cd6bd29509c8
Quratulain-Yasir <hiraiqbal681@gmail.com>
2026-08-17 16:58:05 +0500

    issue reolved by adding mailservices in workspacemodule

--- apps/api/src/workspaces/workspaces.module.ts
+++ apps/api/src/workspaces/workspaces.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
+  imports: [MailModule],
   controllers: [WorkspacesController],
   providers: [WorkspacesService],
 })
